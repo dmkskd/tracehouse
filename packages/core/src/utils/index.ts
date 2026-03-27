1fb4a7ecb9f42c38b8a8c6707a738b7e347757aa
demko <46271281+dmkskd@users.noreply.github.com>
2026-03-27 11:36:04 +0000

    Query Monitor: add health sunburst

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -24,6 +24,7 @@ export { computeMergeEta, pickThroughputEstimate } from './merge-eta.js';
 export type { MergeEtaInfo } from './merge-eta.js';
 export { deriveHealth, mergeThroughputHealth, worstHealth, isMergeStuck } from './merge-health.js';
 export type { Health, HealthNode, ThroughputMap } from './merge-health.js';
+export { deriveQueryHealth, isQueryStuck } from './query-health.js';
 export { detectTimestamp, timestampToDate, formatCell } from './format-cell.js';
 export type { TimestampUnit } from './format-cell.js';
 export { pearson, spearman, crossCorrelation, crossCorrelationDetail, rollingCorrelation, minMaxNormalize, normalizePanelData, correlateToFocused, correlationToOpacity, correlationStrength, computeInsightsAndLags, interpretCorrelation, CORRELATION_ALGORITHMS, CORRELATION_THRESHOLDS, ROLLING_WINDOW_TRIGGER, ROLLING_WINDOW_THRESHOLD } from './correlation.js';
